87063276330a78a32a0c8f2ed6233daf30b2871a
Batmoose <ixnay0@gmail.com>
2013-09-28 18:22:29 -0700

    master

--- syncAsync.js
+++ syncAsync.js
@@ -1,36 +1,47 @@
 var EventEmitter = require('events').EventEmitter,
 globalContext = this;
 module.exports = function syncAsync(){
-    var alreadyDone = false;
-    const emitter = new EventEmitter();
-    _parallel.apply(globalContext, arguments);
-    return nextSync(syncAsync);
+    const firstEmitter = new EventEmitter(),
+    firstVarFn = _variablify(true);
+    return aux(firstEmitter, firstVarFn).apply(globalContext, arguments);
     
-    function nextSync(callback){
+    function aux(anEmitter, varFn){
+        const newVarFn = _variablify(false),
+        newEmitter = new EventEmitter(),
+        next = nextSync(parallel(newEmitter, newVarFn), anEmitter, varFn);
         return function(){
-            const nextSyncArgs = arguments;
-            emitter.once('done', callbackWithArgs);
-            if(alreadyDone)
-                callbackWithArgs();
-            function callbackWithArgs(){
-                callback.apply(globalContext, nextSyncArgs);
-            }
+            next.apply(globalContext, arguments);
+            return aux(newEmitter, newVarFn);
         };
     }
-    function _parallel(){
-        const args = Array.prototype.slice.call(arguments),
-        counter = _countify(arguments.length, function(){
-            emitter.emit('done');
-            alreadyDone = true;
-        });
-        args.forEach(function(fn){
-            if(typeof fn === 'function'){
-                const increment = _countify(1, counter); //insures that counter will only increment once per "fn"
-                process.nextTick(function(){
-                    fn(increment);
-                });
+    function parallel(anEmitter, done){
+        return function(){
+            const args = Array.prototype.slice.call(arguments),
+            counter = _countify(arguments.length, function(){
+                console.log('finished executing');
+                anEmitter.emit('done');
+                done(true);
+            });
+            args.forEach(function(fn){
+                if(typeof fn === 'function'){
+                    const increment = _countify(1, counter); //insures that counter will only increment once per "fn"
+                    process.nextTick(function(){
+                        fn(increment);
+                    });
+                }
+            });
+        };
+    }
+    function nextSync(after, anEmitter, done){
+        return function(){
+            const nextSyncArgs = arguments;
+            anEmitter.once('done', callbackWithArgs);
+            if(done())
+                callbackWithArgs();
+            function callbackWithArgs(){
+                after.apply(globalContext, nextSyncArgs);
             }
-        });
+        };
     }
     
     /*
@@ -43,11 +54,19 @@ module.exports = function syncAsync(){
         callback = callback || def;
         return function(fn){
             if(i > 0){
-                (fn || def)(); i--;
+                (fn || def)();
             }
-            if(i == 0){
+            i--;
+            if(i === 0){
                 callback();
             }
+        };
+    }
+    function _variablify(currentVal){
+        return function(newVal){
+            if(newVal)
+               currentVal = newVal;
+            return currentVal;
         };
     }
 };
