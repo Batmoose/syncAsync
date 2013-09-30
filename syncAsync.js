024bc8c7cf98e0693e798724df5ddf99e6b11681
Batmoose <ixnay0@gmail.com>
2013-09-29 23:14:47 -0700

    Now possible to input arrays!

--- syncAsync.js
+++ syncAsync.js
@@ -1,30 +1,43 @@
 var EventEmitter = require('events').EventEmitter,
 globalContext = this;
 module.exports = function syncAsync(){
+    const stepper = nextSync,
+    onDone = parallel,
+    getNext = function(newEmitter, newVarFn, anEmitter, varFn, counter){
+        return stepper(onDone(newEmitter, newVarFn, counter), anEmitter, varFn);
+    }
     return aux(new EventEmitter(), _variablify(true)).apply(globalContext, arguments);
     
-    function aux(anEmitter, varFn){
+    function aux(anEmitter, varFn, counter){
         const newVarFn = _variablify(false),
         newEmitter = new EventEmitter(),
-        next = nextSync(parallel(newEmitter, newVarFn), anEmitter, varFn);
+        next = getNext(newEmitter, newVarFn, anEmitter, varFn, counter);
         return function(){
             next.apply(globalContext, arguments);
-            return aux(newEmitter, newVarFn);
+            return aux(newEmitter, newVarFn, counter);
         };
     }
-    function parallel(anEmitter, done){
+    function parallel(anEmitter, done, passedCounter){
         return function(){
             const args = Array.prototype.slice.call(arguments),
-            counter = _countify(arguments.length, function(){
+            counter = _countify(arguments.length, 
+             passedCounter ? function(){
                 anEmitter.emit('done');
                 done(true);
-            });
+                passedCounter();
+            } : function(){
+                anEmitter.emit('done');
+                done(true);
+            }),
+            varInnerAux = _variablify(aux(new EventEmitter(), _variablify(true), counter));
             args.forEach(function(fn){
                 if(typeof fn === 'function'){
                     const increment = _countify(1, counter); //insures that counter will only increment once per "fn"
                     process.nextTick(function(){
                         fn(increment);
                     });
+                }else if(fn.constructor === Array){
+                    varInnerAux(varInnerAux().apply(globalContext, fn));
                 }
             });
         };
@@ -59,6 +72,13 @@ module.exports = function syncAsync(){
             }
         };
     }
+    
+    /*
+    * Will return a function that, when executed without
+    * arguments, returns currentVal. If an argument is
+    * passed, currentVal will be set to the value of the
+    * argument.
+    */
     function _variablify(currentVal){
         return function(newVal){
             if(newVal)
